57ba77b31bf826054025cdfb3fc72952e86bea94
Taeuk Kang <taeuk.me@gmail.com>
2018-03-18 21:27:37 +0900

    Initial commit (1.0.0)

---
+++ README.md
@@ -0,0 +1,48 @@
+# google-home-push
+Send push notifications to your Google Home device.
+
+## Installation
+```
+$ npm install --save google-home-push
+```
+
+## Usage
+```
+const GoogleHome = require("google-home-push");
+
+// Pass the name or IP address of your device
+const myHome = new GoogleHome("Living Room Home");
+
+myHome.speak("Hello world!");
+myHome.push("https://example.com/music.mp3");
+```
+
+## API
+### new GoogleHome(deviceIdentifier, [options])
+Creates an `instantce` of GoogleHome.
+
+#### deviceIdentifier
+Type: `string`
+Accepts valid IP addresses or device name.
+
+#### options
+Type: `Object`
+
+##### language
+Default: `en`
+
+##### accent
+Default: `en`
+
+### .speak(message)
+#### message
+Type: `string`
+Input what you want to notify
+
+### .push(url)
+#### url
+Type: `string`
+Enter a valid media URL to cast
+
+## License
+[MIT](../blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const debug = require('debug')('ghome-push');
+debug('starting up...')
+
+const GoogleCastClient = require('castv2-client').Client;
+const DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
+const mdns = require('mdns');
+const browser = mdns.createBrowser(mdns.tcp('googlecast'));
+const googleTTS = require('google-tts-api');
+const isIp = require('is-ip');
+
+class GoogleHome {
+    constructor(deviceIdentifier, options = {}) {
+        this.device = {};
+
+        if (isIp(deviceIdentifier)) {
+            debug('deviceIdentifier as IP address');
+            this.device.ip = deviceIdentifier;
+        } else {
+            debug('deviceIdentifier as device name');
+            this.device.name = deviceIdentifier;
+        }
+        this.device.identifier = deviceIdentifier;
+
+        this.options = options;
+
+        this.options.language = (this.options.language === undefined) ? 'en' : options.language;
+        this.options.accent = (this.options.accent === undefined) ? 'en' : options.accent;
+    }
+
+    searchDevice(name = this.device.name) {
+        return new Promise((resolve, reject) => {
+            browser.start();
+            debug('google cast browser started');
+
+            browser.on('serviceUp', (service) => {
+                browser.stop();
+
+                // Only use the first IP address in the array
+                const address = service.addresses[0];
+                console.log(`Device ${service.txtRecord.fn} at ${address}:${service.port} found`);
+                debug(`found device ${service.txtRecord.fn} at ${address}:${service.port}`);
+
+                if (service.txtRecord.fn.includes(name)) {
+                    resolve(address);
+                } else {
+                    debug(`unable to find device with deviceIdentifier`);
+                    return reject('Unable to find device');
+                }
+            });
+        });
+    }
+
+    async speak(message) {
+        return new Promise((resolve, reject) => {
+            googleTTS(message, this.options.language, 1, 3000, this.options.accent).then((url) => {
+                debug('pushing Google TTS speech data');
+                this.push(url)
+                    .then(resolve)
+                    .catch(reject);
+            }).catch(reject); 
+        });
+    }
+
+    push(url) {
+        return new Promise(async (resolve, reject) => {
+            if (this.device.ip === undefined || !isIp(this.device.ip)) {
+                try {
+                    this.device.ip = await this.searchDevice();
+                } catch(err) {
+                    return reject(err)
+                }
+            }
+    
+            const client = new GoogleCastClient();
+            client.connect(this.device.ip, () => {
+                client.launch(DefaultMediaReceiver, (err, player) => {
+                    const media = {
+                        contentId: url,
+                        contentType: 'audio/mp3',
+                        streamType: 'BUFFERED'
+                    };
+    
+                    player.load(media, { autoplay: true }, (err, status) => {
+                        client.close();
+                        debug(`Google Cast Client closed, successfully pushed data to device`)
+                        console.log(`Pushed to device at ${this.device.ip}`);
+                    });
+                });
+            });
+
+            client.on('error', (err) => {
+                debug(`Google Cast Client error: ${err}`);
+                reject(`Google Cast Client error:\n${err}`);
+                client.close();
+            });
+        });
+    }
+};
+
+module.exports = GoogleHome;
