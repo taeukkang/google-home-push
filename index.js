80ad99534fd5e710474461f6b297f68a17e52513
Taeuk Kang <mail@taeuk.me>
2018-11-28 17:12:35 +0900

    Reformat code

--- index.js
+++ index.js
@@ -1,91 +1,106 @@
-'use strict';
+"use strict";
 
-const GoogleCastClient = require('castv2-client').Client;
-const DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
-const mdns = require('mdns');
-const browser = mdns.createBrowser(mdns.tcp('googlecast'));
-const googleTTS = require('google-tts-api');
-const isIp = require('is-ip');
+const GoogleCastClient = require("castv2-client").Client;
+const DefaultMediaReceiver = require("castv2-client").DefaultMediaReceiver;
+const mdns = require("mdns");
+const browser = mdns.createBrowser(mdns.tcp("googlecast"));
+const googleTTS = require("google-tts-api");
+const isIp = require("is-ip");
 
 class GoogleHome {
-    constructor(deviceIdentifier, options = {}) {
-        this.device = {};
+  constructor(deviceIdentifier, options = {}) {
+    this.device = {};
 
-        if (isIp(deviceIdentifier)) {
-            this.device.ip = deviceIdentifier;
+    if (isIp(deviceIdentifier)) {
+      this.device.ip = deviceIdentifier;
+    } else {
+      this.device.name = deviceIdentifier;
+    }
+    this.device.identifier = deviceIdentifier;
+
+    this.options = options;
+
+    this.options.language =
+      this.options.language === undefined ? "en" : options.language;
+    this.options.accent =
+      this.options.accent === undefined ? "en" : options.accent;
+  }
+
+  searchDevice(name = this.device.name) {
+    return new Promise((resolve, reject) => {
+      browser.start();
+
+      browser.on("serviceUp", service => {
+        browser.stop();
+
+        // Only use the first IP address in the array
+        const address = service.addresses[0];
+        console.log(
+          `Device ${service.txtRecord.fn} at ${address}:${service.port} found`
+        );
+
+        if (service.txtRecord.fn.includes(name)) {
+          resolve(address);
         } else {
-            this.device.name = deviceIdentifier;
+          return reject("Unable to find device");
         }
-        this.device.identifier = deviceIdentifier;
+      });
+    });
+  }
 
-        this.options = options;
+  async speak(message, language) {
+    return new Promise((resolve, reject) => {
+      googleTTS(
+        message,
+        language ? language : this.options.language,
+        1,
+        3000,
+        this.options.accent
+      )
+        .then(url => {
+          this.push(url)
+            .then(resolve)
+            .catch(reject);
+        })
+        .catch(reject);
+    });
+  }
 
-        this.options.language = (this.options.language === undefined) ? 'en' : options.language;
-        this.options.accent = (this.options.accent === undefined) ? 'en' : options.accent;
-    }
+  push(url) {
+    return new Promise(async (resolve, reject) => {
+      if (this.device.ip === undefined || !isIp(this.device.ip)) {
+        try {
+          this.device.ip = await this.searchDevice();
+        } catch (err) {
+          return reject(err);
+        }
+      }
 
-    searchDevice(name = this.device.name) {
-        return new Promise((resolve, reject) => {
-            browser.start();
+      const client = new GoogleCastClient();
+      client.connect(
+        this.device.ip,
+        () => {
+          client.launch(DefaultMediaReceiver, (err, player) => {
+            const media = {
+              contentId: url,
+              contentType: "audio/mp3",
+              streamType: "BUFFERED"
+            };
 
-            browser.on('serviceUp', (service) => {
-                browser.stop();
-
-                // Only use the first IP address in the array
-                const address = service.addresses[0];
-                console.log(`Device ${service.txtRecord.fn} at ${address}:${service.port} found`);
-
-                if (service.txtRecord.fn.includes(name)) {
-                    resolve(address);
-                } else {
-                    return reject('Unable to find device');
-                }
+            player.load(media, { autoplay: true }, (err, status) => {
+              client.close();
+              console.log(`Pushed to device at ${this.device.ip}`);
             });
-        });
-    }
+          });
+        }
+      );
 
-    async speak(message, language) {
-        return new Promise((resolve, reject) => {
-            googleTTS(message, language ? language : this.options.language, 1, 3000, this.options.accent).then((url) => {
-                this.push(url)
-                    .then(resolve)
-                    .catch(reject);
-            }).catch(reject); 
-        });
-    }
-
-    push(url) {
-        return new Promise(async (resolve, reject) => {
-            if (this.device.ip === undefined || !isIp(this.device.ip)) {
-                try {
-                    this.device.ip = await this.searchDevice();
-                } catch(err) {
-                    return reject(err)
-                }
-            }
-    
-            const client = new GoogleCastClient();
-            client.connect(this.device.ip, () => {
-                client.launch(DefaultMediaReceiver, (err, player) => {
-                    const media = {
-                        contentId: url,
-                        contentType: 'audio/mp3',
-                        streamType: 'BUFFERED'
-                    };
-    
-                    player.load(media, { autoplay: true }, (err, status) => {
-                        client.close();
-                        console.log(`Pushed to device at ${this.device.ip}`);
-                    });
-                });
-            });
-
-            client.on('error', (err) => {
-                reject(`Google Cast Client error:\n${err}`);
-                client.close();
-            });
-        });
-    }
-};
+      client.on("error", err => {
+        reject(`Google Cast Client error:\n${err}`);
+        client.close();
+      });
+    });
+  }
+}
 
 module.exports = GoogleHome;
