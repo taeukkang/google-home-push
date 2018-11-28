8f8ada0638c258a309ced06afc7eecc2bd007a29
Taeuk Kang <mail@taeuk.me>
2018-11-28 17:12:06 +0900

    Remove debug()'s

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const debug = require('debug')('ghome-push');
-debug('starting up...')
-
 const GoogleCastClient = require('castv2-client').Client;
 const DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
 const mdns = require('mdns');
@@ -15,10 +12,8 @@ class GoogleHome {
         this.device = {};
 
         if (isIp(deviceIdentifier)) {
-            debug('deviceIdentifier as IP address');
             this.device.ip = deviceIdentifier;
         } else {
-            debug('deviceIdentifier as device name');
             this.device.name = deviceIdentifier;
         }
         this.device.identifier = deviceIdentifier;
@@ -32,7 +27,6 @@ class GoogleHome {
     searchDevice(name = this.device.name) {
         return new Promise((resolve, reject) => {
             browser.start();
-            debug('google cast browser started');
 
             browser.on('serviceUp', (service) => {
                 browser.stop();
@@ -40,12 +34,10 @@ class GoogleHome {
                 // Only use the first IP address in the array
                 const address = service.addresses[0];
                 console.log(`Device ${service.txtRecord.fn} at ${address}:${service.port} found`);
-                debug(`found device ${service.txtRecord.fn} at ${address}:${service.port}`);
 
                 if (service.txtRecord.fn.includes(name)) {
                     resolve(address);
                 } else {
-                    debug(`unable to find device with deviceIdentifier`);
                     return reject('Unable to find device');
                 }
             });
@@ -55,7 +47,6 @@ class GoogleHome {
     async speak(message, language) {
         return new Promise((resolve, reject) => {
             googleTTS(message, language ? language : this.options.language, 1, 3000, this.options.accent).then((url) => {
-                debug('pushing Google TTS speech data');
                 this.push(url)
                     .then(resolve)
                     .catch(reject);
@@ -84,14 +75,12 @@ class GoogleHome {
     
                     player.load(media, { autoplay: true }, (err, status) => {
                         client.close();
-                        debug(`Google Cast Client closed, successfully pushed data to device`)
                         console.log(`Pushed to device at ${this.device.ip}`);
                     });
                 });
             });
 
             client.on('error', (err) => {
-                debug(`Google Cast Client error: ${err}`);
                 reject(`Google Cast Client error:\n${err}`);
                 client.close();
             });
